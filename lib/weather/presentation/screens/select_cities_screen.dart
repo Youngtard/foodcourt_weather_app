ec9779824b1362fee1aed1e36410a4f52808157b
Femi Sotonwa <tolusotonwa@yahoo.com>
2024-03-07 18:15:55 +0100

    Adjust initial cities selection UI

--- lib/weather/presentation/screens/select_cities_screen.dart
+++ lib/weather/presentation/screens/select_cities_screen.dart
@@ -70,7 +70,7 @@ class _SelectCitiesScreenState extends State<SelectCitiesScreen> {
                         ),
                         Text(
                           _selectedCities.length < _minimumCities
-                              ? "Kindly select at least ${_getCitiesLeft()} more ${_getCitiesLeft() == 1 ? "city" : "cities"} to proceed"
+                              ? "Select at least ${_getCitiesLeft()} more ${_getCitiesLeft() == 1 ? "city" : "cities"} to proceed"
                               : _selectedCities.map((e) => e.name).join(", "),
                           style: textTheme.bodyLarge!.copyWith(
                             fontWeight: FontWeight.w400,
@@ -89,62 +89,64 @@ class _SelectCitiesScreenState extends State<SelectCitiesScreen> {
                         const SizedBox(
                           height: 72,
                         ),
-                        Wrap(
-                          spacing: 16.0,
-                          runSpacing: 16.0,
-                          children: cities.mapIndexed(
-                            (index, city) {
-                              final isSelected = _selectedCities.contains(city);
+                        Center(
+                          child: Wrap(
+                            spacing: 16.0,
+                            runSpacing: 16.0,
+                            children: cities.mapIndexed(
+                              (index, city) {
+                                final isSelected = _selectedCities.contains(city);
 
-                              return SingleChildScrollView(
-                                child: InkWell(
-                                  borderRadius: BorderRadius.circular(100),
-                                  onTap: () {
-                                    if (isSelected) {
-                                      if (city.name != "Lagos") {
-                                        _selectedCities.remove(city);
+                                return SingleChildScrollView(
+                                  child: InkWell(
+                                    borderRadius: BorderRadius.circular(100),
+                                    onTap: () {
+                                      if (isSelected) {
+                                        if (city.name != "Lagos") {
+                                          _selectedCities.remove(city);
+                                        }
+                                      } else {
+                                        _selectedCities.add(city);
                                       }
-                                    } else {
-                                      _selectedCities.add(city);
-                                    }
-                                    setState(() {});
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(100),
-                                      border: Border.all(
-                                        color: isSelected ? kPrimaryColor : kNeutral900.withOpacity(0.5),
+                                      setState(() {});
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        border: Border.all(
+                                          color: isSelected ? kPrimaryColor : kNeutral900.withOpacity(0.5),
+                                        ),
+                                        color: isSelected ? kPrimaryColor : Colors.white,
                                       ),
-                                      color: isSelected ? kPrimaryColor : Colors.white,
-                                    ),
-                                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        Flexible(
-                                          child: Text(
-                                            city.name,
-                                            style: textTheme.bodyMedium!.copyWith(
-                                              color: isSelected ? Colors.white : kNeutral900,
+                                      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          Flexible(
+                                            child: Text(
+                                              city.name,
+                                              style: textTheme.bodyMedium!.copyWith(
+                                                color: isSelected ? Colors.white : kNeutral900,
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                        const SizedBox(
-                                          width: 8,
-                                        ),
-                                        Icon(
-                                          isSelected ? Icons.check : Icons.add,
-                                          color: isSelected ? kNeutral900.withOpacity(0.8) : null,
-                                          size: 16,
-                                        ),
-                                      ],
+                                          const SizedBox(
+                                            width: 8,
+                                          ),
+                                          Icon(
+                                            isSelected ? Icons.check : Icons.add,
+                                            color: isSelected ? kNeutral900.withOpacity(0.8) : null,
+                                            size: 16,
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                              );
-                            },
-                          ).toList(),
+                                );
+                              },
+                            ).toList(),
+                          ),
                         ),
                       ],
                     ),
